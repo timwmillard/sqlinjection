9e03a996572d216cee8ee5ec9ff1efcadd0400bd
Tim Millard <tim@rapidpaylegal.com>
2022-12-15 08:27:40 +1100

    Move func

--- main.go
+++ main.go
@@ -90,3 +90,18 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+func dbURI() string {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName,
+	)
+}
